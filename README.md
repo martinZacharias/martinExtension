f4cac626f5453940d60b4a15ce48b33736069ceb
MartinZa99 <pickolo04@gmail.com>
2022-10-19 14:46:35 +0200

    added designMode toggle

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 -   NewTab page with customizable names, links and icons
 -   "Save" context menu entry to instantly download media skipping the "Save As" location prompt
+-   DesignMode toggle context menu entry
--- background.js
+++ background.js
@@ -5,16 +5,40 @@ chrome.contextMenus.create({
 	contexts: ["image", "video", "audio"],
 });
 
+chrome.contextMenus.create({
+	id: "toggleDesignMode",
+	title: "Design mode",
+	contexts: ["page", "editable"],
+});
+
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-	if (info.menuItemId === "saveFile") {
-		chrome.downloads.download({ url: info.srcUrl }, (downloadId) => {
-			if (info.pageUrl === info.srcUrl) {
-				chrome.tabs.remove(tab.id);
-			}
-			setTimeout(() => {
-				chrome.downloads.setShelfEnabled(false);
-				chrome.downloads.setShelfEnabled(true);
-			}, 1000);
-		});
+	switch (info.menuItemId) {
+		case "saveFile":
+			chrome.downloads.download({ url: info.srcUrl }, (downloadId) => {
+				if (info.pageUrl === info.srcUrl) {
+					chrome.tabs.remove(tab.id);
+				}
+				setTimeout(() => {
+					chrome.downloads.setShelfEnabled(false);
+					chrome.downloads.setShelfEnabled(true);
+				}, 1000);
+			});
+			break;
+
+		case "toggleDesignMode":
+			chrome.scripting.executeScript({
+				target: { tabId: tab.id },
+				func: () => {
+					if (document.designMode === "on") {
+						document.designMode = "off";
+					} else {
+						document.designMode = "on";
+					}
+				},
+			});
+			break;
+
+		default:
+			throw `Unknown menuItemId :${info.menuItemId}`;
 	}
 });
