663737f57e1eed8f936050f6ce397ed8fc9f7290
martinZacharias <mrt.zacharias@gmail.com>
2022-12-29 15:17:12 +0100

    added newtab pages import/export

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 const storage = chrome.storage.local;
 const options = {};
 
+const statusSpan = document.querySelector("#status");
 const optionsForm = document.querySelector("#optionsForm");
 
 storage.get(["options"]).then(({ options }) => {
-	const { hideDownloadShelfTime } = options;
+	const { hideDownloadShelfTime } = options ?? {};
 	// default to 1000 if undefined
 	optionsForm.hideDownloadShelfTime.value = hideDownloadShelfTime ?? 1000;
 });
@@ -13,3 +14,48 @@ optionsForm.hideDownloadShelfTime.addEventListener("change", (event) => {
 	options.hideDownloadShelfTime = parseInt(event.target.value);
 	storage.set({ options });
 });
+
+const bookmarksFileInput = document.querySelector("#bookmarksFile");
+bookmarksFileInput.addEventListener("change", (event) => {
+	event.preventDefault();
+	try {
+		const file = bookmarksFileInput.files[0];
+		const reader = new FileReader();
+		reader.addEventListener("load", (event) => {
+			const pages = JSON.parse(event.target.result);
+			storage.set({ pages });
+			statusSpan.textContent = "Imported pages!";
+		});
+		reader.readAsText(file);
+	} catch (error) {
+		statusSpan.textContent = "Invalid file!";
+	}
+});
+
+const importBookmarksButton = document.querySelector("#importBookmarks");
+importBookmarksButton.addEventListener("click", (event) =>
+	bookmarksFileInput.click()
+);
+
+const exportBookmarksButton = document.querySelector("#exportBookmarks");
+exportBookmarksButton.addEventListener("click", (event) => {
+	storage.get(["pages"]).then(({ pages }) => {
+		const output = JSON.stringify(pages);
+		if (output) {
+			saveFile("pages.json", output, "application/json");
+			statusSpan.textContent = "Download started!";
+		} else {
+			statusSpan.textContent = "Failed to save pages!";
+		}
+	});
+});
+
+function saveFile(filename, data, type) {
+	const blob = new Blob([data], { type });
+	const url = URL.createObjectURL(blob);
+
+	const a = document.createElement("a");
+	a.href = url;
+	a.download = filename;
+	a.click();
+}
